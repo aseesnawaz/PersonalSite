644db62b7a3fff8dd9a590bf40975f8c6cab4d5a
Asees Nawaz <aseesnawaz01@gmail.com>
2017-05-13 11:49:48 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//
+//	jQuery Validate example script
+//
+//	Prepared by David Cochran
+//
+//	Free for your use -- No warranties, no guarantees!
+//
+
+
+// slideshow images
+// var imgs = []
+//
+// function storeImages(base, first, last){
+// 	for (let i = first; i < last+1; i++){
+// 		let image = base + "/" + i + '.jpg';
+// 		imgs.push(image);
+// 	}
+// }
+//
+// //Access slide show image object and change every 2 seconds
+// var imageIndex = 1;
+// function changeImage(){
+// 	let image = "url(" + imgs[imageIndex] + ")";
+// 	document.getElementById('slideshow').style.animationTimingFunction = "ease-in";
+// 	document.getElementById('slideshow').style.animationDuration = "2s";
+// 	document.getElementById('slideshow').style.backgroundImage = image;
+// 	imageIndex = (imageIndex + 1);
+// 	if(imageIndex == imgs.length){imageIndex = 0;}
+// }
+// storeImages('img',1,5);
+// setInterval(changeImage, 5000);
